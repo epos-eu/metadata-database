3857a55c8110f8f49258caf41a0a07b7d48b8f4f
Valerio Vinciarelli <valerio.vinciarelli@gmail.com>
2023-09-17 15:55:11 +0200

    little update

--- epos-processing-db/processing_environment_db.sql
+++ epos-processing-db/processing_environment_db.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS public.processing_unit
     processing_environment_type_id character varying(1024) NOT NULL,
     creation_time timestamptz NOT NULL,
     PRIMARY KEY (id)
-    );
+);
 
 CREATE TABLE IF NOT EXISTS public.resource_item
 (
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS public.resource_item
     resource_url character varying(1024),
     file_name character varying(1024),
     PRIMARY KEY (id)
-    );
+);
 
 ALTER TABLE IF EXISTS  public.resource_item
     ADD FOREIGN KEY (processing_unit_id)
