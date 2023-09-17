46b0fd10fec96fa5563a5d2667c1ce6c00c9ccfa
Valerio Vinciarelli <valerio.vinciarelli@gmail.com>
2023-09-17 15:42:51 +0200

    update processing tables

--- epos-processing-db/processing_environment_db.sql
+++ epos-processing-db/processing_environment_db.sql
@@ -1,14 +1,5 @@
 BEGIN;
 
-CREATE TABLE IF NOT EXISTS public.processing_environment
-(
-    id character varying(1024) NOT NULL,
-    name character varying(1024) NOT NULL,
-    description text NOT NULL,
-    PRIMARY KEY (id)
-    );
-
-
 CREATE TABLE IF NOT EXISTS public.processing_unit
 (
     id character varying(1024) NOT NULL,
@@ -21,11 +12,6 @@ CREATE TABLE IF NOT EXISTS public.processing_unit
     creation_time timestamptz NOT NULL,
     PRIMARY KEY (id)
     );
-
-ALTER TABLE IF EXISTS  public.processing_unit
-    ADD FOREIGN KEY (processing_environment_type_id)
-    references public.processing_environment (id);
-
 
 CREATE TABLE IF NOT EXISTS public.resource_item
 (
@@ -41,22 +27,5 @@ CREATE TABLE IF NOT EXISTS public.resource_item
 ALTER TABLE IF EXISTS  public.resource_item
     ADD FOREIGN KEY (processing_unit_id)
     references public.processing_unit (id);
-
-CREATE TABLE IF NOT EXISTS public.icsd_user
-(
-    id character varying(1024) NOT NULL,
-    email character varying(1024),
-    edu_identifier character varying(1024) NOT NULL,
-    first_name character varying(1024),
-    last_name character varying(1024),
-    PRIMARY KEY (id)
-    );
-
-ALTER TABLE IF EXISTS  public.processing_unit
-    ADD FOREIGN KEY (icsd_user_id)
-    references public.icsd_user (id);
-
-INSERT INTO public.processing_environment (id, name, description) VALUES ('jupyter_identifier', 'Swirrl Api Jupyter', 'Software for Interactive Reproducible Research Labs - an API enabling science gateways to create reproducible workspaces that integrate notebooks and analysis tools.');
-INSERT INTO public.processing_environment (id, name, description) VALUES ('enlighten_identifier', 'Swirrl Api Enlighten', 'Enlighten web is a visual data analysis and visualization application. The application is implemented as a web application.');
 
 END;
