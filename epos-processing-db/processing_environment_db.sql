e7b319137850de7efffc0dd63d5933f3ef586ca3
Valerio Vinciarelli <valerio.vinciarelli@gmail.com>
2023-11-07 10:29:02 +0100

    update processing environment db

--- epos-processing-db/processing_environment_db.sql
+++ epos-processing-db/processing_environment_db.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+CREATE TYPE processing_unit_status AS ENUM ('READY', 'NOT READY');
+
 CREATE TABLE IF NOT EXISTS public.processing_unit
 (
     id character varying(1024) NOT NULL,
@@ -10,6 +12,9 @@ CREATE TABLE IF NOT EXISTS public.processing_unit
     icsd_user_id character varying(1024) NOT NULL,
     processing_environment_type_id character varying(1024) NOT NULL,
     creation_time timestamptz NOT NULL,
+    changed_time timestamptz NOT NULL,
+    status processing_unit_status NOT NULL,
+    additional_information character varying(10485760),
     PRIMARY KEY (id)
 );
 
@@ -21,6 +26,7 @@ CREATE TABLE IF NOT EXISTS public.resource_item
     resource_uid character varying(1024) NOT NULL,
     resource_url character varying(1024),
     file_name character varying(1024),
+    file_type character varying(1024),
     PRIMARY KEY (id)
 );
 
