483a2d78cdb52bfa1b57811169729193a3a97838
Valerio Vinciarelli <valerio.vinciarelli@gmail.com>
2024-02-09 10:55:41 +0100

    add max connection

--- build/build.sql
+++ build/build.sql
@@ -4,5 +4,8 @@
  SET standard_conforming_strings = off;
  SET escape_string_warning = off;
 
+ALTER SYSTEM SET max_connections ='5000';
+select pg_reload_conf();
+
  CREATE DATABASE "cerif"  OWNER = postgres;
 
