1a671ef45910181c602688986de312b52651a3a4
Valerio Vinciarelli <valerio.vinciarelli@gmail.com>
2024-04-03 15:34:50 +0200

    update

--- epos-converter-db/plugins_registry.sql
+++ epos-converter-db/plugins_registry.sql
@@ -4,10 +4,10 @@ CREATE TABLE plugin (
 	id character varying(1024) NOT NULL PRIMARY KEY,
 	software_source_code_id VARCHAR(1024) NOT NULL,
 	software_application_id VARCHAR(1024) NOT NULL,
-	version VARCHAR(20) NOT NULL,
-	proxy_type VARCHAR(25) NOT NULL,
-	runtime VARCHAR(25) NOT NULL,
-	execution VARCHAR(25) NOT NULL,
+	version VARCHAR(1024) NOT NULL,
+	proxy_type VARCHAR(1024) NOT NULL,
+	runtime VARCHAR(1024) NOT NULL,
+	execution VARCHAR(1024) NOT NULL,
 	installed BOOLEAN NOT NULL DEFAULT FALSE,
 	enabled BOOLEAN NOT NULL DEFAULT FALSE
 );
