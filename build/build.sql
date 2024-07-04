256cc56892f003ae99e4715d98aaf14b0ca4a93a
Valerio Vinciarelli <valerio.vinciarelli@gmail.com>
2024-07-04 21:50:47 +0200

    Update

--- build/build.sql
+++ build/build.sql
@@ -4,5 +4,8 @@
  SET standard_conforming_strings = off;
  SET escape_string_warning = off;
 
+ ALTER SYSTEM SET max_connections = 5000;
+ ALTER SYSTEM RESET shared_buffers;
+
  CREATE DATABASE cerif  OWNER = postgres;
 
