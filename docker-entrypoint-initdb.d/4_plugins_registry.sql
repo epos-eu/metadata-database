7d808f968c526d11306e9a60e9af7ea1af065ae6
vvalerio <valerio.vinciarelli@epos-eric.eu>
2024-10-07 12:44:24 +0200

    update

---
+++ docker-entrypoint-initdb.d/4_plugins_registry.sql
@@ -0,0 +1,24 @@
+\connect cerif;
+BEGIN;
+
+CREATE TABLE plugin (
+	id character varying(1024) NOT NULL PRIMARY KEY,
+	software_source_code_id VARCHAR(1024) NOT NULL,
+	software_application_id VARCHAR(1024) NOT NULL,
+	version VARCHAR(1024) NOT NULL,
+	proxy_type VARCHAR(1024) NOT NULL,
+	runtime VARCHAR(1024) NOT NULL,
+	execution VARCHAR(1024) NOT NULL,
+	installed BOOLEAN NOT NULL DEFAULT FALSE,
+	enabled BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE plugin_relations (
+	id character varying(1024) NOT NULL PRIMARY KEY,
+	plugin_id character varying(1024) NOT NULL,
+	relation_id VARCHAR(1024) NOT NULL,
+	relation_type VARCHAR(1024) NOT NULL
+);
+
+
+END;
