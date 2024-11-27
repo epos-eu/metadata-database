69fbf4a62e39f139c61388e37b197d19c56b98c6
vvalerio <valerio.vinciarelli@epos-eric.eu>
2024-11-27 10:24:43 +0100

    update

--- docker-entrypoint-initdb.d/10_build.sql
+++
@@ -1,8 +0,0 @@
-\connect postgres
-
- SET client_encoding = 'UTF8';
- SET standard_conforming_strings = off;
- SET escape_string_warning = off;
- 
- CREATE DATABASE processing_catalogue  OWNER = postgres;
-
--- docker-entrypoint-initdb.d/7_build.sql
+++
@@ -1,8 +0,0 @@
-\connect postgres
-
- SET client_encoding = 'UTF8';
- SET standard_conforming_strings = off;
- SET escape_string_warning = off;
-
- CREATE DATABASE converter_catalogue  OWNER = postgres;
-
